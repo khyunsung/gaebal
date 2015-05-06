5130af51cb42db322648b7cdef94d680f77ca08b
Kim Hyunsung <khyunsung@naver.com>
2015-05-06 17:36:05 +0900

    5/6 마스터 브렌치 릴리즈

---
+++ Debug/booting.pp
@@ -0,0 +1,65 @@
+# FIXED
+
+booting.obj: ../booting.C
+booting.obj: ../define.h
+booting.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Device.h
+booting.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Adc.h
+booting.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_DevEmu.h
+booting.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_CpuTimers.h
+booting.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_ECan.h
+booting.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_ECap.h
+booting.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_DMA.h
+booting.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_EPwm.h
+booting.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_EQep.h
+booting.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Gpio.h
+booting.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_I2c.h
+booting.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_McBSP.h
+booting.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_PieCtrl.h
+booting.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_PieVect.h
+booting.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Spi.h
+booting.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Sci.h
+booting.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_SysCtrl.h
+booting.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_XIntrupt.h
+booting.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Xintf.h
+booting.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/include/DSP2833x_GlobalPrototypes.h
+booting.obj: C:/ti/ccsv5/tools/compiler/c2000_6.1.0/include/math.h
+booting.obj: C:/ti/ccsv5/tools/compiler/c2000_6.1.0/include/linkage.h
+booting.obj: C:/ti/ccsv5/tools/compiler/c2000_6.1.0/include/float.h
+booting.obj: C:/ti/ccsv5/tools/compiler/c2000_6.1.0/include/stdio.h
+booting.obj: C:/ti/ccsv5/tools/compiler/c2000_6.1.0/include/stdarg.h
+booting.obj: C:/ti/ccsv5/tools/compiler/c2000_6.1.0/include/stdlib.h
+booting.obj: C:/ti/ccsv5/tools/compiler/c2000_6.1.0/include/stdlibf.h
+booting.obj: ../extern_variable.h
+booting.obj: ../extern_prototype.h
+
+../booting.C: 
+../define.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Device.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Adc.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_DevEmu.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_CpuTimers.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_ECan.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_ECap.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_DMA.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_EPwm.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_EQep.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Gpio.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_I2c.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_McBSP.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_PieCtrl.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_PieVect.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Spi.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Sci.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_SysCtrl.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_XIntrupt.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/include/DSP2833x_Xintf.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/include/DSP2833x_GlobalPrototypes.h: 
+C:/ti/ccsv5/tools/compiler/c2000_6.1.0/include/math.h: 
+C:/ti/ccsv5/tools/compiler/c2000_6.1.0/include/linkage.h: 
+C:/ti/ccsv5/tools/compiler/c2000_6.1.0/include/float.h: 
+C:/ti/ccsv5/tools/compiler/c2000_6.1.0/include/stdio.h: 
+C:/ti/ccsv5/tools/compiler/c2000_6.1.0/include/stdarg.h: 
+C:/ti/ccsv5/tools/compiler/c2000_6.1.0/include/stdlib.h: 
+C:/ti/ccsv5/tools/compiler/c2000_6.1.0/include/stdlibf.h: 
+../extern_variable.h: 
+../extern_prototype.h: 
